ddd46d4598791325ff92021d7e4b7d518b4c3981
Yasin Vəliyev <68180570+YasinVeliyev@users.noreply.github.com>
2020-10-28 15:40:30 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 keys=[ "A", "S", "D", "F", "G", "H", "J","W", "E", "T", "Y", "U"]
 function Key(event){
     if (keys.includes(event.key.toUpperCase())){
-        let audio=new Audio(`music/${event.key.toUpperCase()}.mp3`)
+        let audio=new Audio(`Music/${event.key.toUpperCase()}.mp3`)
         audio.type="audio/mpeg"
         audio.play()}
 
