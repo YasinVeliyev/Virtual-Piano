d5be77ace0c7785113754910ef4050dd7d4c7461
Yasin Vəliyev <68180570+YasinVeliyev@users.noreply.github.com>
2020-10-28 13:56:01 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+keys=[ "A", "S", "D", "F", "G", "H", "J","W", "E", "T", "Y", "U"]
+function Key(event){
+    if (keys.includes(event.key.toUpperCase())){
+        let audio=new Audio(`music/${event.key.toUpperCase()}.mp3`)
+        audio.type="audio/mpeg"
+        audio.play()}
+
+    else{
+        console.log(`Wrong '${event.key}' key is pressed.`)
+    }
+}
+
+document.addEventListener("keypress",(event)=>Key(event))
